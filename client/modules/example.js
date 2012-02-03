ecc71c0958bf6580c847c6517cd713d98bc60d3d
Matt Holden <matt@tapengage.com>
2012-02-03 01:38:35 -0800

    Fix indentation.

--- client/modules/example.js
+++ client/modules/example.js
@@ -1,12 +1,12 @@
 (function(Example) {
 
-  Example.Model = Backbone.Model.extend({ /* ... */ });
-  Example.Collection = Backbone.Collection.extend({ /* ... */ });
-  Example.Router = Backbone.Router.extend({ /* ... */ });
+    Example.Model = Backbone.Model.extend({ /* ... */ });
+    Example.Collection = Backbone.Collection.extend({ /* ... */ });
+    Example.Router = Backbone.Router.extend({ /* ... */ });
 
 
-  Example.View = Backtester.TemplateView.extend({
-    template: "example"
-  });
+    Example.View = Backtester.TemplateView.extend({
+        template: "example"
+    });
 
 })(Backtester.module("example"));
--- client/namespace.js
+++ client/namespace.js
@@ -1,51 +1,51 @@
 this.Backtester = {
 
-  // Keep active application instances namespaced under an app object.
-  app: _.extend({}, Backbone.Events),
+    // Keep active application instances namespaced under an app object.
+    app: _.extend({}, Backbone.Events),
 
 
-  // Organize logical components of code into modules.
-  module: function() {
-    // Internal module cache.
-    var modules = {};
+    // Organize logical components of code into modules.
+    module: function() {
+        // Internal module cache.
+        var modules = {};
 
-    // Create a new empty module or load an existing module.
-    return function(name) {
-      if (modules[name]) {
-        return modules[name];
-      }
-      return modules[name] = {};
-    };
-  }(),
+        // Create a new empty module or load an existing module.
+        return function(name) {
+            if (modules[name]) {
+                return modules[name];
+            }
+            return modules[name] = {};
+        };
+    }(),
 
 
-  // Base class for a View that renders a JST template using its model values.
-  TemplateView: Backbone.View.extend({
+    // Base class for a View that renders a JST template using its model values.
+    TemplateView: Backbone.View.extend({
 
-    render: function () {
+        render: function () {
 
-      var JST = window.JST || {};
-      if (!JST) {
-        throw new Error('JST templates not found')
-      }
-      if (!this.template) {
-        throw new Error('TemplateView must define a template');
-      }
+            var JST = window.JST || {};
+            if (!JST) {
+                throw new Error('JST templates not found')
+            }
+            if (!this.template) {
+                throw new Error('TemplateView must define a template');
+            }
 
-      var template = JST[this.template];
-      if (!template) {
-        throw new Error('Template not found: ' + this.template);
-      }
+            var template = JST[this.template];
+            if (!template) {
+                throw new Error('Template not found: ' + this.template);
+            }
 
-      var values = this.model ? this.model.toJSON() : {};
-      var html = template(values);
-      this.$el.html(html);
+            var values = this.model ? this.model.toJSON() : {};
+            var html = template(values);
+            this.$el.html(html);
 
-      return this;
-    },
+            return this;
+        },
 
-    renderTo: function (target) {
-      $(target).html(this.render().el);
-    }
-  })
+        renderTo: function (target) {
+            $(target).html(this.render().el);
+        }
+    })
 };
