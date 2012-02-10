32aa183b225de2d40e7cf1ac9948acfecac09ac9
Matt Holden <matt@tapengage.com>
2012-02-09 18:29:05 -0800

    Refactor Quotes a bit for clarity.

--- client/modules/Quotes.js
+++ client/modules/Quotes.js
@@ -5,41 +5,22 @@
  */
 (function(Quotes) {
 
-
 	var TimeSeries = Backtester.module('TimeSeries');
 
-
 	/**
-	 * Extend TimeSeries to specialize it for monthly stock quotes.
-	 */
-	Quotes.TimeSeries = TimeSeries.Model.extend({
-
-		initialize: function (attrs) {
-
-			// Use the ticker as the model id.
-			this.set({
-				'id': this.get('ticker')
-			});
-
-			TimeSeries.Model.prototype.initialize.apply(this, arguments);
-		},
-
-		// Override getValues to convert cents to dollars.
-		getValues: function () {
-			return _.map(this.get('values'), function (value) {
-				return value / 100.0;
-			});
-		}
-	});
-
-
-	/**
-	 * Fetch/manage a collection of montly closing price TimeSeries for one or more tickers.
+	 * Fetch a Collection of monthly closing price TimeSeries for one or more tickers.
 	 */
 	Quotes.MonthlyQuotes = TimeSeries.Collection.extend({
-
-		model: Quotes.TimeSeries,
 		baseUrl: '/quotes/monthly/?s=',
+
+		// Convert cents to dollars.
+		model: TimeSeries.Model.extend({
+			getValues: function () {
+				return _.map(this.get('values'), function (value) {
+					return value / 100.0;
+				});
+			}
+		}),
 
 		initialize: function (models, options) {
 			options = options || {};
@@ -56,15 +37,24 @@
 	});
 
 
-	// Cache all monthly quotes results fetched from the server.
-	Quotes.cache = new Backbone.Collection();
 
+	// Privately cache a Collection of monthly quotes TimeSeries models for some given tickers.
+	var cache = new Backbone.Collection();
 
-	// Return a Collection from the cache for an array of tickers.
-	// Throw an error if any ticker isn't in the cache.
-	Quotes.cache.getCollection = function (tickers) {
+	// Use tickers as ids when cacheing, so we can lookup by ticker.
+	cache.add = function (models) {
+		_.each(models, function (model) {
+			model.set('id', model.get('ticker'));
+		});
+		return Backbone.Collection.prototype.add.call(this, models);
+	};
+
+	// Return a TimeSeries.Collection for some given tickers.
+	// Throw an error if any ticker hasn't already been cached.
+	cache.getCollection = function (tickers) {
+
 		var models = _.map(tickers, function (ticker) {
-			var model = Quotes.cache.get(ticker);
+			var model = cache.get(ticker);
 			if (!model) {
 				throw new Error('Model not found in cache: ' + ticker);
 			}
@@ -72,6 +62,7 @@
 		});
 		return new TimeSeries.Collection(models);
 	};
+
 
 	/**
 	 * Fetch and cache monthly quotes for an array of tickers.
@@ -81,28 +72,28 @@
 		options = options || {};
 
 		// Get the tickers we don't have in the cache.
-		var cached = Quotes.cache.pluck('id'),
-			needed = _.difference(tickers, cached);
+		var cached  = cache.pluck('id'),
+			missing = _.difference(tickers, cached);
 
 		console.log('Cached: [' + cached + ']');
 
 		// If everything we need is already cached, just return the collection.
-		if (needed.length === 0) {
-			options.success(Quotes.cache.getCollection(tickers));
+		if (missing.length === 0) {
+			options.success(cache.getCollection(tickers));
 			return;
 		}
 
-		console.log('Fetching: [' + needed + ']');
+		console.log('Fetching: [' + missing + ']');
 
 		// Otherwise, fetch what we need to add to the cache.
 		var collection = new Quotes.MonthlyQuotes([], {
-			'tickers': needed
+			'tickers': missing
 		});
 		collection.fetch({
 			success: function (collection) {
-				// Save the returned results in the cache, and return the desired collection.
-				Quotes.cache.add(collection.models);
-				options.success(Quotes.cache.getCollection(tickers));
+				// Save the fetched results in the cache, and return the complete collection.
+				cache.add(collection.models);
+				options.success(cache.getCollection(tickers));
 			},
 			error: options.error || function () {
 				alert('Error!');
