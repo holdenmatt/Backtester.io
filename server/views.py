78d4ad6fa97f50cf2d06de86f7fa9519f7d25da6
Matt Holden <matt@tapengage.com>
2012-02-11 16:14:54 -0800

    Delete about/home templates; turn on page cacheing

--- server/views.py
+++ server/views.py
@@ -8,28 +8,16 @@ from app import app
 
 @app.route('/')
 def home():
-    """Render home page."""
-    return render_template('home.html')
-
-@app.route('/about/')
-def about():
-    """Render the about page."""
-    return render_template('about.html')
-
-@app.route('/<file_name>.txt')
-def send_text_file(file_name):
-    """Send a static text file."""
-    file_dot_text = file_name + '.txt'
-    return app.send_static_file(file_dot_text)
+    return render_template('base.html')
 
 @app.after_request
 def add_header(response):
     """
     Add headers to force latest IE rendering engine or Chrome Frame,
-    and also to cache the rendered page for 10 minutes.
+    and cache the rendered page for 10 minutes.
     """
     response.headers['X-UA-Compatible'] = 'IE=Edge,chrome=1'
-    # response.headers['Cache-Control'] = 'public, max-age=600'
+    response.headers['Cache-Control'] = 'public, max-age=600'
     return response
 
 @app.errorhandler(404)
